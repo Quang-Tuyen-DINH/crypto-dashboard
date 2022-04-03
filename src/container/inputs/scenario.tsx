a99add800df9d631d324d584c9be852d30c0409c
Quang Tuyen DINH <qtuyen43@gmail.com>
2022-04-03 21:18:20 +0200

    Testing reactive form

--- src/container/inputs/scenario.tsx
+++ src/container/inputs/scenario.tsx
@@ -4,12 +4,20 @@ import {
   FieldGroup,
   FieldControl,
   Validators,
+  AbstractControl,
 } from "react-reactive-form";
 import "./scenario.scss";
 import InputChild from "../../components/input-child/input-child"
 
 export default function Senario() {
   const [botname, setBotname] = React.useState("-");
+  const [scenarioBotM1Form, setBotM1Config] = React.useState(
+    FormBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+      rememberMe: false
+    })
+  )
 
   const fetchData = async() => {
     await fetch('/botname').then(res => res.json()).then(data => {
@@ -18,17 +26,22 @@ export default function Senario() {
   };
 
   const loginForm = FormBuilder.group({
-    username: ["", Validators.required],
-    password: ["", Validators.required],
-    rememberMe: false
-});
+    data: ["", Validators.required],
+    startDate: ["", Validators.required],
+    endDate: ["", Validators.required],
+    timeFrame: ["", Validators.required],
+    currency: ["", Validators.required],
+    riskFreeRate: ["", Validators.required],
+    totalWealth: ["", Validators.required],
+    customerName: ""
+  });
 
   const handleReset = () => {
-      this.loginForm.reset();
+      loginForm.reset();
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
       e.preventDefault();
-      console.log("Form values", this.loginForm.value);
+      console.log("Form values", loginForm.value);
   }
 
   React.useEffect(() => {
@@ -46,30 +59,120 @@ export default function Senario() {
     //     <InputChild label="risk free rate" type="primary" disabled={false} defaultValue="8%"/>
     //     <InputChild label="total wealth" type="primary" disabled={false} defaultValue="10000"/>
     // </div>
-    <FieldGroup
-    control={this.loginForm}
-    render={({ get, invalid }) => (
-      <form onSubmit={this.handleSubmit}>
-
-        <FieldControl
-          name="username"
-          render={TextInput}
-          meta={{ label: "Username" }}
+    <div>
+      <h3 className='title'>scenario for {botname}</h3>
+      <FieldGroup
+      control={loginForm}
+      render={({value, pristine, invalid}) => (
+        <form onSubmit={() => handleSubmit}>
+          <FieldControl
+          name="customerName"
+          render={({
+            handler,
+            pending,
+            touched,
+            hasError
+          }: AbstractControl) => (
+            <div>
+              <label>Name:</label>
+              <input {...handler()} />
+              {pending && <i className="fa fa-spinner fa-spin" />}
+              <div>
+                <span>
+                  {touched &&
+                    hasError("required") &&
+                    "Customer name is required"}
+                </span>
+              </div>
+            </div>
+          )}
         />
-        <button
-          type="button"
-          onClick={this.handleReset}
-        >
-          Reset
-        </button>
-        <button
-          type="submit"
-          disabled={invalid}
-        >
-          Submit
-        </button>
-      </form>
-    )}
-  />
+          <FieldControl
+            name="data"
+            render={({
+              handler,
+              pending,
+              touched,
+              hasError
+            }: AbstractControl) => (
+              <InputChild label="data" type="success" disabled={true} defaultValue="DAILY"/>
+            )}
+          />
+          <FieldControl
+            name="startDate"
+            render={({}: AbstractControl) => (
+              <InputChild label="start date" type="primary" disabled={false} defaultValue=""/>
+            )}
+          />
+          <FieldControl
+            name="endDate"
+            render={({}: AbstractControl) => (
+              <InputChild label="end date" type="primary" disabled={false} defaultValue=""/>
+            )}
+          />
+          <FieldControl
+            name="timeFrame"
+            render={({}: AbstractControl) => (
+              <InputChild label="time frame (days)" type="success" disabled={true} defaultValue="405.00"/>
+            )}
+          />
+          <FieldControl
+            name="currency"
+            render={({}: AbstractControl) => (
+              <InputChild label="currency" type="primary" disabled={false} defaultValue="USD"/>
+            )}
+          />
+          <FieldControl
+            name="riskFreeRate"
+            render={({}: AbstractControl) => (
+              <InputChild label="risk free rate" type="primary" disabled={false} defaultValue="8%"/>
+            )}
+          />
+          <FieldControl
+            name="totalWealth"
+            render={({}: AbstractControl) => (
+              <InputChild label="total wealth" type="primary" disabled={false} defaultValue="10000"/>
+            )}
+          />
+          <FieldControl
+          name="test"
+          render={({
+            handler,
+            pending,
+            touched,
+            hasError
+          }: AbstractControl) => (
+            <div>
+              <label>Name:</label>
+              <input {...handler()} />
+              {pending && <i className="fa fa-spinner fa-spin" />}
+              <div>
+                <span>
+                  {touched &&
+                    hasError("required") &&
+                    "Customer name is required"}
+                </span>
+              </div>
+            </div>
+          )}
+        />
+          <button
+            type="button"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            // disabled={invalid}
+          >
+            Submit
+          </button>
+          <div>{JSON.stringify(loginForm.value)}</div>
+        </form>
+      )}
+    />
+    </div>
   );
 }
--- src/interfaces/botM1.tsx
+++ src/interfaces/botM1.tsx
@@ -1,35 +1,35 @@
 interface botM1 {
-  id: number;
-  config: {
-    nb_assets: number;
-    currency: string
+  id?: number;
+  config?: {
+    nb_assets?: number;
+    currency?: string
   },
-  buy_periodic: {
-    enable: boolean;
-    rsi_min: number
+  buy_periodic?: {
+    enable?: boolean;
+    rsi_min?: number
   },
-  buy_dip: {
-    enable: boolean;
-    rsi_max: number;
-    cool: number;
-    share_reserve: number;
-    share_vault: number
+  buy_dip?: {
+    enable?: boolean;
+    rsi_max?: number;
+    cool?: number;
+    share_reserve?: number;
+    share_vault?: number
   },
-  sell_take_profit: {
-    enable: boolean;
-    rsi_min: number;
-    tp_share: number;
-    cool: number
+  sell_take_profit?: {
+    enable?: boolean;
+    rsi_min?: number;
+    tp_share?: number;
+    cool?: number
   },
-  sell_stop_loss: {
-    enable: boolean;
-    threshold: number;
-    cool: number
+  sell_stop_loss?: {
+    enable?: boolean;
+    threshold?: number;
+    cool?: number
   },
-  funding: {
-    enable: boolean;
-    dca_funding: number;
-    period: string
+  funding?: {
+    enable?: boolean;
+    dca_funding?: number;
+    period?: string
   }
 }
 
